ff47048e8ac4a5583d65df6f4d8bbd48b896e16b
Nicholas Vanderpool <nvanderp@MacBook-Pro.attlocal.net>
2018-01-08 16:04:14 -0500

    Boiler plate fixed and edited!

---
+++ secrets.js
@@ -0,0 +1,3 @@
+process.env.GOOGLE_CLIENT_ID = '1078211730479-h3np3tr9h8mrsquuhb4pmmtgsphgb4e9.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = '_5D2lRPhaE5hxdJRkSqK3Ziy'
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
