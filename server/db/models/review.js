d4aca3dc50f374c255fe7651fa9ca82db92433fa
Jane Volin <janevolin@yahoo.com>
2018-01-08 17:29:54 -0500

    Set up Schema

---
+++ server/db/models/review.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Review = db.define('review', {
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT
+  },
+  rating: {
+    type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Review
--- server/index.js
+++ server/index.js
@@ -91,7 +91,7 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync()
+const syncDb = () => db.sync({force: true})
 
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
