457a0bbddd625ab8c4452acbed8157084711abd7
Nicholas Vanderpool <nvanderp@MacBook-Pro.attlocal.net>
2018-01-09 14:40:07 -0500

    Added basic single-product view and api route for accessing single product

---
+++ client/components/single-product.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {withRouter, Link} from 'react-router-dom'
+
+const SingleProduct = (props) => {
+  const { product } = props
+
+  return (
+    <div>
+      {
+        !product ? null :
+        <div>
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+        </div>
+      }
+    </div>
+  )
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = (state, ownProps) => {
+  const productId = Number(ownProps.match.params.id)
+  return {
+    product: state.products.find(product => product.id = productId)
+  }
+}
+
+export default connect(mapState)(SingleProduct)
+
+/**
+ * PROP TYPES
+ */
+// Main.propTypes = {
+//   children: PropTypes.object,
+//   handleClick: PropTypes.func.isRequired,
+//   isLoggedIn: PropTypes.bool.isRequired
+// }
--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {Route, Switch, Router} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, Products} from './components'
+import {Main, Login, Signup, UserHome, Products, SingleProduct} from './components'
 import {me, fetchProducts} from './store'
 
 /**
@@ -24,7 +24,8 @@ class Routes extends Component {
             {/* Routes placed here are available to all visitors */}
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-            <Route path="/products" component={Products} />
+            <Route exact path="/products" component={Products} />
+            <Route path="/products/:id" component={SingleProduct} />
             {
               isLoggedIn &&
                 <Switch>
